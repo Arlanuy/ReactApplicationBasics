ae63cd559d619ef38acde4999a04ed36be975e3c
Arlan Uy <gnhs.com@gmail.com>
2021-11-16 15:23:50 -0600

    finished different mouse events

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -42,10 +42,24 @@ function BookList() {
 
 const Book = (props) => {
   const {img, title, author} = props
-  return <article className="book">
+  //attribute, eventHandler
+  //onClick, onMouseOver
+  const clickHandler = (e) => {
+    console.log(e.target)
+    alert('hello world');
+  }
+
+  const complexExample = (author) => {
+    console.log(author)
+  }
+  return <article className="book" onMouseOver={() => {
+    console.log(title);
+  }}>
     <img src={img} alt='' height="130" width="120" />
-    <h1>{title}</h1>
+    <h1 onClick = {() => console.log(title)}>{title}</h1>
     <h4>{author}</h4>
+    <button type="button" onClick={clickHandler}>Click here</button>
+    <button type="button" onClick = {() => complexExample(author)}>more complex example</button>
   </article>
 }
 
