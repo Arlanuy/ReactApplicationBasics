a25aba11cecef96d5844bfed4c01dd1a0d253f82
Arlan Uy <gnhs.com@gmail.com>
2021-11-17 15:28:31 -0600

    finish useeffect basics

--- react-advanced-2020-master/src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ react-advanced-2020-master/src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -3,7 +3,22 @@ import React, { useState, useEffect } from 'react';
 // cleanup function
 // second parameter
 const UseEffectBasics = () => {
-  return <h2>useEffect Basics</h2>;
+  const [value, setValue] = useState(0);
+
+  //you can have many other useEffect
+  useEffect(() => {
+    console.log('call useEffect');
+    if (value >= 1) {  
+      document.title = `New Messages(${value})`;
+    }
+    }, [value]) //only runs on initial render when you have this empty array
+  console.log('render component');
+  return (<>
+  <h1>{value}</h1>
+  <button className="btn" onClick={() => setValue(value + 1)}>click me</button>
+  </>
+    
+    );
 };
 
 export default UseEffectBasics;
