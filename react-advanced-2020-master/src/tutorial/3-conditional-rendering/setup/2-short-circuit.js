4bc34e6c9d4223ae9d3ffe7ba041e567c0ccac2e
Arlan Uy <gnhs.com@gmail.com>
2021-11-18 10:25:27 -0600

    finished ternary error message

--- react-advanced-2020-master/src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ react-advanced-2020-master/src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -14,6 +14,11 @@ const ShortCircuit = () => {
     <h1>{text || 'john doe'}</h1>
     <button className="btn" onClick={() => setIsError(!isError)}>toggle error</button>
     {isError && <h1>Error...</h1>}
+    {isError ? <p>there is an error ...</p> : 
+    <div>
+      <h2>there is no error</h2>
+    </div>
+    }
   </>);
 };
 
