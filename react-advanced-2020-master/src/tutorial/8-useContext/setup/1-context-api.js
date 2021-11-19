4a149b653386d150d34a3f447599b618e790ad73
Arlan Uy <gnhs.com@gmail.com>
2021-11-19 10:09:08 -0600

    changed title to custom hooks

--- react-advanced-2020-master/src/tutorial/8-useContext/setup/1-context-api.js
+++ react-advanced-2020-master/src/tutorial/8-useContext/setup/1-context-api.js
@@ -16,7 +16,7 @@ const ContextAPI = () => {
   };
   return (
     <PersonContext.Provider value = {{removePerson, people}}>
-      <h3>prop drilling</h3>
+      <h3>Using context api</h3>
       <List />
     </PersonContext.Provider>
   );
